230e98a748b415c82f9bdc2997af74a71ac7e1da
Your NameVaishnavi <vaishuk38@gmail.com>
2024-08-21 15:13:02 +0530

    new

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 export default function About(){
   const[myStyle,setMyStyle] = useState({
